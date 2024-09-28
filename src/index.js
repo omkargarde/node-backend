c6556e69052fd216bce740d40e5af51e900ff1f2
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-09-28 11:11:19 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+connectDB()
+	.then(() => {
+		app.listen(process.env.PORT || 8000, () => {
+			console.log(`\nServer is running on port::${process.env.PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("\nMONGO DB connection Failed::", error);
+	});
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+export const AsyncHandler = (requestHandler) => async (req, res, next) => {
+	try {
+		await requestHandler(req, res, next);
+	} catch (error) {
+		res.status(error.code || 500).json({
+			success: false,
+			message: error.message,
+		});
+	}
+};
